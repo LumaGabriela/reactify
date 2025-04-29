c4b45a2ae2257ede6aca55c715cdc43c6b017b35
Luma Gabriela <lumagabriela3331@gmail.com>
2025-04-28 21:41:20 -0300

    Update Welcome.jsx

--- resources/js/Pages/Welcome.jsx
+++ resources/js/Pages/Welcome.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Head, Link } from '@inertiajs/react';
-<<<<<<< HEAD
-=======
 import { route } from 'ziggy-js'
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
     return (
