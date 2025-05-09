6a027055994e8b3eb1b61a1a1a8c247c978f6f8f
LumaGabriela <82393096+LumaGabriela@users.noreply.github.com>
2025-05-09 11:56:07 -0300

    feat; adiciona relacao com stories no model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,11 +9,17 @@ class Project extends Model
     protected $fillable = [
         'title',
         'description',
+        'status'
     ];
 
     // Relação: Um projeto pertence a um usuário
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+    
+    public function stories() 
+    {
+        return $this->hasMany(Story::class);
     }
 }
