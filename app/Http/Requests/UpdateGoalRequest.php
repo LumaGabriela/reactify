65f3a73a82d8c4c6946e872728e7fc5a44e15eba
Luma Gabriela <lumagabriela3331@gmail.com>
2025-05-16 18:01:02 -0300

    fix: corrige as regras

--- app/Http/Requests/UpdateGoalRequest.php
+++ app/Http/Requests/UpdateGoalRequest.php
@@ -1,30 +1,31 @@
 <?php
 
 namespace App\Http\Requests;
-
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateGoalRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
+  /**
+   * Determine if the user is authorized to make this request.
+   */
+  public function authorize(): bool
+  {
+    return true;
+  }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
-    {
-        return [
-            'title' => 'nullable|string:max:255',
-            'type' => 'nullable|in:user,system',
-            'priority' => 'nullable|in:low,medium,high,urgent',
-        ];
-    }
+  /**
+   * Get the validation rules that apply to the request.
+   *
+   * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+   */
+  public function rules(): array
+  {
+    return [
+      'title' => 'nullable|string:max:255',
+      'type' => 'nullable|in:cg,bg',
+      'priority' => 'nullable|in:low,medium,high,urgent',
+    ];
+  }
+
+
 }
