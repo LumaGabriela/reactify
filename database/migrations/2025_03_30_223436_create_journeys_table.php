16c6f1a275c740ebe129327cf61d90ad2a0ee3b1
Luma Gabriela <lumagabriela3331@gmail.com>
2025-05-16 23:38:04 -0300

    fix: corrige as colunas da tabela de journeys

--- database/migrations/2025_03_30_223436_create_journeys_table.php
+++ database/migrations/2025_03_30_223436_create_journeys_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('journeys', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->jsonb('steps');
+            $table->foreignId('project_id')->constrained('projects');
             $table->timestamps();
         });
     }
