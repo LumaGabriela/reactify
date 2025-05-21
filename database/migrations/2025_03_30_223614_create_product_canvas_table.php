9b1243621ab0f98b757ad262abb0f72be89aa673
Luma Gabriela <lumagabriela3331@gmail.com>
2025-05-20 21:39:29 -0300

    fix: migration corrigida

--- database/migrations/2025_03_30_223614_create_product_canvas_table.php
+++ database/migrations/2025_03_30_223614_create_product_canvas_table.php
@@ -6,55 +6,63 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
-    {
-        Schema::create('product_canvas', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('project_id')->constrained('projects');
-            $table->timestamps();
-        });
+  /**
+   * Run the migrations.
+   */
+  public function up(): void
+  {
+    Schema::create('product_canvas', function (Blueprint $table) {
+      $table->id();
+      $table->string('description');
+      $table->string('issue');
+      $table->string('solution');
+      $table->string('persona');
 
-        Schema::create('canvas_personas', function (Blueprint $table) {
-          $table->foreignId('product_canvas_id')->constrained('product_canvas');
-        });
+      $table->string('restriction');
+      $table->string('is');
 
-        Schema::create('canvas_solutions', function (Blueprint $table) {
-          $table->foreignId('product_canvas_id')->constrained('product_canvas');
-        });
+      $table->foreignId('project_id')->constrained('projects');
+      $table->timestamps();
+    });
 
-        Schema::create('canvas_issues', function (Blueprint $table) {
-          $table->foreignId('product_canvas_id')->constrained('product_canvas');
-        });
+    //   Schema::create('canvas_personas', function (Blueprint $table) {
+    //     $table->foreignId('product_canvas_id')->constrained('product_canvas');
+    //   });
 
-        Schema::create('canvas_restrictions', function (Blueprint $table) {
-          $table->foreignId('product_canvas_id')->constrained('product_canvas');
-        });
+    //   Schema::create('canvas_solutions', function (Blueprint $table) {
+    //     $table->foreignId('product_canvas_id')->constrained('product_canvas');
+    //   });
 
-        Schema::create('canvas_is', function (Blueprint $table) {
-          $table->foreignId('product_canvas_id')->constrained('product_canvas');
-        });
+    //   Schema::create('canvas_issues', function (Blueprint $table) {
+    //     $table->foreignId('product_canvas_id')->constrained('product_canvas');
+    //   });
 
-        Schema::create('canvas_is_not', function (Blueprint $table) {
-          $table->foreignId('product_canvas_id')->constrained('product_canvas');
-        });
-    }
+    //   Schema::create('canvas_restrictions', function (Blueprint $table) {
+    //     $table->foreignId('product_canvas_id')->constrained('product_canvas');
+    //   });
+
+    //   Schema::create('canvas_is', function (Blueprint $table) {
+    //     $table->foreignId('product_canvas_id')->constrained('product_canvas');
+    //   });
+
+    //   Schema::create('canvas_is_not', function (Blueprint $table) {
+    //     $table->foreignId('product_canvas_id')->constrained('product_canvas');
+    //   });
+  }
 
 
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('product_canvas');
-        Schema::dropIfExists('canvas_personas');
-        Schema::dropIfExists('canvas_solutions');
-        Schema::dropIfExists('canvas_issues');
-        Schema::dropIfExists('canvas_restrictions');
-        Schema::dropIfExists('canvas_is');
-        Schema::dropIfExists('canvas_is_not');
-    }
+  /**
+   * Reverse the migrations.
+   */
+  public function down(): void
+  {
+    Schema::dropIfExists('product_canvas');
+    // Schema::dropIfExists('canvas_personas');
+    // Schema::dropIfExists('canvas_solutions');
+    // Schema::dropIfExists('canvas_issues');
+    // Schema::dropIfExists('canvas_restrictions');
+    // Schema::dropIfExists('canvas_is');
+    // Schema::dropIfExists('canvas_is_not');
+  }
 };
