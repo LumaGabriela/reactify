620139f2ed0596b6bce977203ef39ba84da71715
Luma Gabriela <lumagabriela3331@gmail.com>
2025-07-23 15:04:50 -0300

    Update SendInvitationTest.php

--- tests/Feature/SendInvitationTest.php
+++ tests/Feature/SendInvitationTest.php
@@ -1,13 +1,50 @@
 <?php
 
-use App\Mail\ProjectInvitationMail;
 use App\Models\User;
 use App\Models\Project;
 use App\Models\ProjectInvitation;
+use App\Notifications\UserInvitedToProject;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Notification;
 
-test('sending project invitation', function () {
-  Mail::fake();
+test('sending project invitation notification', function () {
+
+  $owner = User::factory()->create();
+  $userToInvite = User::factory()->create();
+  $project = Project::factory()->create();
+  $project->users()->attach($owner->id, ['role' => 'admin']);
+
+  $response = $this
+    ->actingAs($owner)
+    ->from(route('dashboard'))
+    ->post(route('projects.invitations.store', $project->id), [
+      'email' => $userToInvite->email,
+      'role' => 'member',
+    ]);
+
+
+  $invite = ProjectInvitation::where('email', $userToInvite->email)->first();
+
+  $response
+    ->assertSessionHas('status', 'success')
+    ->assertSessionHas('message', 'Invitation sent successfully')
+    ->assertRedirect(route('dashboard'));
+
+  //testa o armazenamento do convite no banco de dados
+  $this
+    ->assertDatabaseHas('notifications', [
+      'notifiable_id' => $userToInvite->id,
+      'type' => UserInvitedToProject::class,
+    ])
+    ->assertDatabaseHas('project_invitations', [
+      'email' => $userToInvite->email,
+      'project_id' => $project->id,
+      'role' => 'member',
+    ]);
+});
+
+test('sending project invitation notification ', function () {
+  Notification::fake();
 
   // 2. Crie os dados necessários para este teste específico.
   $owner = User::factory()->create();
@@ -22,21 +59,34 @@ test('sending project invitation', function () {
       'email' => $userToInvite->email,
       'role' => 'member',
     ]);
+  //testa o envio do e-mail
+  Notification::assertSentTo(
+    $userToInvite,
+    UserInvitedToProject::class,
+    function ($notification, $channels) use ($userToInvite, $project) {
+      $this->assertContains('mail', $channels, 'o canal de email nao foi usado');
+      $this->assertContains('database', $channels, 'o canal de banco de dados nao foi usado');
 
-  $invite = ProjectInvitation::where('email', $userToInvite->email)->first();
+      $mailData = $notification->toMail($userToInvite);
+      // dump($mailData);
+
+      $dbData = $notification->toArray($userToInvite);
+      // dump($dbData);
+
+      return true;
+    }
+  );
 
   $response
     ->assertSessionHas('status', 'success')
     ->assertSessionHas('message', 'Invitation sent successfully')
     ->assertRedirect(route('dashboard'));
 
-  $this->assertDatabaseHas('project_invitations', [
-    'email' => $userToInvite->email,
-    'project_id' => $project->id,
-    'role' => 'member',
-  ]);
-
-  Mail::assertSent(ProjectInvitationMail::class, function ($mail) use ($userToInvite) {
-    return $mail->hasTo($userToInvite->email);
-  });
+  //testa o armazenamento do convite no banco de dados
+  $this
+    ->assertDatabaseHas('project_invitations', [
+      'email' => $userToInvite->email,
+      'project_id' => $project->id,
+      'role' => 'member',
+    ]);
 });
