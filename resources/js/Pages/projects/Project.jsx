78e2bcf39eb199a5af428ba8479b1c3e6a9d644c
Katlen Vanessa <94211156+KatlenVanessa@users.noreply.github.com>
2025-07-26 15:59:43 -0300

    feat: cerimonia refining

--- resources/js/Pages/projects/Project.jsx
+++ resources/js/Pages/projects/Project.jsx
@@ -26,6 +26,12 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion'
 import { ProjectPermissions } from '@/Components/ProjectPermissions'
 
 const ProjectView = ({ projectDB = [], page = 'inception' }) => {
@@ -419,20 +425,16 @@ const ProjectView = ({ projectDB = [], page = 'inception' }) => {
       id: 'US10',
       title: 'Gerenciamento de Perfil de Usuário',
       description:
-        'Como usuário, quero poder gerenciar meu perfil para que eu possa manter minhas informações atualizadas e personalizar minha experiência.',
+        'Eu como Leon Cardoso quero uma seção no painel de controle administrativo que exibe estatísticas, gráficos ou tabelas de visitas à plataforma para consultar o fluxo de visitas à plataforma.',
       userStories: [
         {
           id: 'US10.1',
-          title: 'Como usuário, quero poder editar minhas informações básicas.',
+          title: 'Eu como Leon Cardoso. quero uma seção no painel de controle administrativo que gere gráficos ou tabelas com os dados do fluxo de visitas à plataforma para consultar o fluxo de visitas à plataforma.',
         },
         {
           id: 'US10.2',
-          title: 'Como usuário, quero poder alterar minha senha.',
-        },
-        {
-          id: 'US10.3',
-          title: 'Como usuário, quero poder carregar uma foto de perfil.',
-        },
+          title: 'Eu como Leon Cardoso quero uma seção no painel de controle administrativo que exibe o número de acessos totais e uma média aritmética de acessos diários para consultar o fluxo de visitas à plataforma.',
+        }
       ],
     }
 
@@ -442,7 +444,7 @@ const ProjectView = ({ projectDB = [], page = 'inception' }) => {
           <h3 className="text-xl font-bold text-primary mb-2">
             {epicStory.id}
           </h3>
-          <p className="text-foreground">{epicStory.description}</p>
+          <p className="text-foreground text-sm">{epicStory.description}</p>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
@@ -461,12 +463,137 @@ const ProjectView = ({ projectDB = [], page = 'inception' }) => {
       </div>
     )
   }
-  const BusinessRules = () => (
-    <div className="w-full text-center p-4">Business Rules Content</div>
-  )
-  const UseScenarios = () => (
-    <div className="w-full text-center p-4">Use Scenarios Content</div>
-  )
+  const BusinessRules = () => {
+    const businessRules = {
+      id: 'US10',
+      description:
+        'Esta é uma User Story',
+      userStories: [
+        {
+          id_rn: 'RN01',
+          title: 'Esta é uma regra de negócio identificada através da User Story US10.',
+          id_us: 'US10'
+        },
+        {
+          id_rn: 'RN02',
+          title: 'Esta é outra regra de negócio identificada através da User Story US10',
+          id_us: 'US10'
+        }
+      ],
+    }
+
+    return (
+      <div className="p-4">
+        <div className="bg-card border border-primary rounded-lg p-4 mb-6 shadow-lg">
+          <h3 className="text-xl font-bold text-primary mb-2">
+            {businessRules.id}
+          </h3>
+          <p className="text-foreground text-sm">{businessRules.description}</p>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {businessRules.userStories.map((story, index) => (
+            <div
+              key={story.id_rn}
+              className="bg-muted rounded-lg p-4 border border-border"
+            >
+              <h4 className="font-semibold text-foreground mb-1 text-sm">
+                {story.id_rn}
+              </h4>
+              <p className="text-sm text-muted-foreground">{story.title}</p>
+              <br></br>
+               <h4 className="font-semibold text-foreground mb-1 text-sm">
+                {story.id_us}
+              </h4>
+            </div>
+          ))}
+        </div>
+      </div>
+    )
+  }
+  const UseScenarios = () => {
+    const scenarios = [
+      {
+        id: 'US14',
+        userStory:
+          'Eu como Leon Cardoso quero uma opção de exclusão de filmes do catálogo da plataforma no painel de controle administrativo para remover um filme do catálogo.',
+        scenario: {
+          title: 'Administrador quer excluir um filme do catálogo',
+          given:
+            'que o Administrador está autenticado no painel de controle administrativo da plataforma.',
+          when: 'o Administrador seleciona a opção de exclusão para um filme do catálogo.',
+          then1:
+            'o sistema exibe uma mensagem de confirmação solicitando a confirmação da exclusão.',
+          and: 'o Administrador Cardoso confirma a exclusão.',
+          then2:
+            'uma mensagem de é exibida indicando que o filme foi removido com sucesso.',
+        },
+      },
+    ]
+
+    return (
+      <div className="p-4">
+        {scenarios.map((scenarioData) => (
+          <div
+            key={scenarioData.id}
+            className="bg-card border border-border rounded-lg p-4 mb-6 shadow-lg"
+          >
+            <div className="bg-muted p-4 rounded-lg mb-4">
+              <p className="font-semibold text-sm text-xl font-bold text-primary">
+                {scenarioData.id}
+              </p>
+              <p className="font-semibold text-sm">
+                {scenarioData.userStory}
+              </p>
+            </div>
+
+            <Accordion
+              type="single"
+              collapsible
+              className="w-full"
+              defaultValue="item-1"
+            >
+              <AccordionItem
+                value="item-1"
+                className="border bg-muted text-muted-foreground rounded-lg px-4"
+              >
+                <AccordionTrigger className="font-semibold text-sm">
+                  AS: {scenarioData.scenario.title}
+                </AccordionTrigger>
+                <AccordionContent className="px-2">
+                  <ul className="list-disc pl-5 space-y-2 text-sm text-left">
+                    <li>
+                      <span className="font-semibold">GIVEN</span>{' '}
+                      {scenarioData.scenario.given}
+                    </li>
+                    <li>
+                      <span className="font-semibold">WHEN</span>{' '}
+                      {scenarioData.scenario.when}
+                    </li>
+                    <li>
+                      <span className="font-semibold">THEN</span>{' '}
+                      {scenarioData.scenario.then1}
+                    </li>
+                    <li>
+                      <span className="font-semibold">AND</span>{' '}
+                      {scenarioData.scenario.and}
+                    </li>
+                    <li>
+                      <span className="font-semibold">THEN</span>{' '}
+                      {scenarioData.scenario.then2}
+                    </li>
+                  </ul>
+                  <div className="text-right text-sm font-semibold mt-4">
+                    {scenarioData.id}
+                  </div>
+                </AccordionContent>
+              </AccordionItem>
+            </Accordion>
+          </div>
+        ))}
+      </div>
+    )
+  }
 
   const renderRefiningContent = () => {
     switch (activeRefiningMenu) {
