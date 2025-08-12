2a907841f9fddaeaab298c0f81c41bc388a05443
Luma Gabriela <lumagabriela3331@gmail.com>
2025-08-12 20:32:04 -0300

    feat: adiciona componentes reutilizaveis

---
+++ resources/js/Components/InfoButton.jsx
@@ -0,0 +1,53 @@
+import { cn } from '@/lib/utils'
+import { Info } from 'lucide-react'
+const InfoButton = ({
+  data = {
+    title: 'Info',
+    description: 'This is an info button',
+    classNames: {
+      badge: '',
+      button: '',
+      content: '',
+    },
+  },
+
+  badgeContent = null,
+}) => {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          className={cn(
+            ' flex items-center justify-center gap-2 p-2 rounded-lg text-foreground bg-card hover:bg-muted transition-colors',
+            data.classNames.button,
+          )}
+        >
+          {badgeContent && (
+            <Badge
+              variant="outline"
+              className={cn(
+                `${data.classNames.badge}`,
+                'border-0 text-primary-foreground',
+              )}
+            >
+              {badgeContent}
+            </Badge>
+          )}
+          {data.title}
+          <Info className="text-muted-foreground" size={18} />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent
+        className={cn(
+          'bg-popover text-popover-foreground ',
+          data.classNames.content,
+        )}
+      >
+        {data.description}
+        <PopoverArrow className="fill-popover" />
+      </PopoverContent>
+    </Popover>
+  )
+}
+
+export default InfoButton
