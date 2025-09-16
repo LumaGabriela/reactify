9865415eda6b06c62c98ef98dde2fff55396ca51
Luma Gabriela <lumagabriela3331@gmail.com>
2025-09-16 20:51:37 -0300

    fix: corrige nome da tabela

--- database/migrations/2025_07_15_193549_project_invitations_table.php
+++ database/migrations/2025_07_15_193549_project_invitations_table.php
@@ -30,6 +30,6 @@ return new class extends Migration
    */
   public function down(): void
   {
-    Schema::dropIfExists('projecct_invitations');
+    Schema::dropIfExists('project_invitations');
   }
 };
